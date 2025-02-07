11fc281c6425cf27d9b8390a05c540ef14f75262
CJ Avilla <cj.avilla@samsara.com>
2025-02-07 17:23:33 -0500

    Add webhook handling

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -15,9 +15,56 @@ class WebhooksController < ApplicationController
       source: params[:source],
       payload: params.to_unsafe_h,
       status: :pending,
-      event_type: params[:event_type],
+      event_type: params[:eventType],
     )
 
+    handle_webhook_event(event)
+  rescue => e
+    event.update(status: :failed, processing_errors: e.message)
+  ensure
     render json: { message: "Webhook received" }, status: :ok
   end
+
+  private
+
+  def handle_webhook_event(event)
+    case event.event_type
+    when "VehicleUpdated"
+      vehicle = Vehicle.find_by(samsara_id: event.payload["data"]["vehicle"]["id"])
+      vehicle.update!(
+        name: event.payload["data"]["vehicle"]["name"],
+        notes: event.payload["data"]["vehicle"]["notes"],
+      )
+      event.update(status: :processed)
+    when "AddressUpdated"
+      address = Address.find_by(samsara_id: event.payload["data"]["address"]["id"])
+      address.update!(
+        name: event.payload["data"]["address"]["name"],
+        formatted_address: event.payload["data"]["address"]["formattedAddress"],
+        latitude: event.payload["data"]["address"]["latitude"],
+        longitude: event.payload["data"]["address"]["longitude"],
+        geofence: event.payload["data"]["address"]["geofence"],
+      )
+      event.update(status: :processed)
+    when "AddressCreated"
+      address = Address.find_or_initialize_by(samsara_id: event.payload["data"]["address"]["id"])
+      address.update!(
+        name: event.payload["data"]["address"]["name"],
+        formatted_address: event.payload["data"]["address"]["formattedAddress"],
+        latitude: event.payload["data"]["address"]["latitude"],
+        longitude: event.payload["data"]["address"]["longitude"],
+        geofence: event.payload["data"]["address"]["geofence"],
+      )
+      event.update(status: :processed)
+    when "DriverUpdated"
+      driver = Driver.find_by(samsara_id: event.payload["data"]["driver"]["id"])
+      driver.update!(
+        name: event.payload["data"]["driver"]["name"],
+        username: event.payload["data"]["driver"]["username"],
+        status: event.payload["data"]["driver"]["driverActivationStatus"],
+      )
+      event.update(status: :processed)
+    end
+  end
 end
+
